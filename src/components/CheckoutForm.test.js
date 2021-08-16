159ba16b1fcdaf95e8c72081eba25f4537a55bdc
Mallory <mallorydoescoding@gmail.com>
2021-08-15 20:34:41 -0500

    Testing done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -5,6 +5,42 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
 
-test("shows success message on submit with form details", () => {});
+    render(<CheckoutForm/>)
+
+    const header = screen.getByText('Checkout Form')
+
+    expect(header).toBeInTheDocument()
+
+});
+
+test("shows success message on submit with form details", () => {
+
+    render(<CheckoutForm/>)
+
+    const firstName = screen.getByLabelText('First Name:')
+    userEvent.type(firstName, 'Mallory')
+
+    const lastName = screen.getByLabelText('Last Name:')
+    userEvent.type(lastName, 'Clouse')
+
+    const address = screen.getByLabelText('Address:')
+    userEvent.type(address, '73 Warwick Rd')
+
+    const city = screen.getByLabelText('City:')
+    userEvent.type(city, 'Little Rock')
+
+    const state = screen.getByLabelText('State:')
+    userEvent.type(state, 'Arkansas')
+
+    const zipcode = screen.getByLabelText('Zip:')
+    userEvent.type(zipcode, '72205')
+
+    const checkout = screen.getByRole('button')
+    userEvent.click(checkout)
+
+    const confirmation = screen.getByTestId('successMessage')
+    expect(confirmation).toBeInTheDocument()
+    
+});
